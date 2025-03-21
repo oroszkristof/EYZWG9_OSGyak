3e6f94427b4913825f4f3ad64c54234f0f23a853
Orosz Kristóf <kristof.orosz.11@gmail.com>
2025-03-21 20:18:40 +0100

    5.Pract

---
+++ EYZWG9_0319/eyzwg9_patient.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+int main(int argc, char *argv[])
+{
+    pid_t ppid, pid, cpid;
+    ppid = getpid();
+    printf("Szulo PID: %d\n", ppid);
+    
+    pid = fork();
+    if (pid < 0) {
+        printf("Fork hiba");
+        return 1;
+    } else if (pid == 0) {
+        cpid = getpid();
+        printf("Gyermek PID: %d\n", cpid);
+        execl("./eyzwg9_child", "eyzwg9_child", (char *)NULL);
+        printf("Execl hiba");
+        return 1;
+    } else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            printf("Waitpid hiba");
+            return 1;
+        }
+        printf("A gyermek futása befejezodott, visszateresi ertek: %d\n", status);
+        return 0;
+    }
+}
